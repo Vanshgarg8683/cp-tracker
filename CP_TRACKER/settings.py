5035fcb77abb80716f65f95ebf2da66b7b6a4040
= <vg9233384@gmail.com>
2026-05-25 01:25:45 +0530

    again again again again new Fixed deployment and API issues

--- CP_TRACKER/settings.py
+++ CP_TRACKER/settings.py
@@ -141,9 +141,12 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 from datetime import timedelta
 
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
-    ),
+    'DEFAULT_PARSER_CLASSES': [
+        'rest_framework.parsers.JSONParser',
+    ],
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+    ]
 }
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
